7dec2b6c9f3057bc3f24e6a78ba703f11c0433cb
maria laura <marialauramoreto@hotmail.com>
2016-10-27 10:02:04 -0200

    inclusao

---
+++ SICE/src/main/java/br/edu/ifsp/arq/sice/config/init/AppInitializer.java
@@ -0,0 +1,39 @@
+package br.edu.ifsp.arq.sice.config.init;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import br.edu.ifsp.arq.sice.config.WebConfig;
+
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+	//configuração de caracteres
+		@Override
+		protected Filter[] getServletFilters() {
+			CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+			characterEncodingFilter.setEncoding("UTF-8");
+			characterEncodingFilter.setForceEncoding(true);
+			
+			return new Filter[] { characterEncodingFilter };
+		}
+
+}
